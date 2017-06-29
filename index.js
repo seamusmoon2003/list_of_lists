56f7f3d958bf2767afdd0d0fbbe108861df01931
seamusmoon2003 <seamusmoon2003@gmail.com>
2017-06-29 10:51:29 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var docClient = new AWS.DynamoDB.DocumentClient( {apiVersion: '2012-08-10'});
 
 // ssml-builder stuff
 var Speech = require('ssml-builder');
-var speech = new Speech();
+//var speech = new Speech();
 /* Example code for using ssml-builder:
 Also, check out the AmazonSpeech part (see readme), that
 can give Amazon specific speech like whisper.
@@ -34,7 +34,7 @@ this.emit(':tell', speechOutput);
 // UserID (S: String), List Name (S: String), List Entries (L: List) 
 // It would be great if the List Entries field could be a set. Need
 // to figure out how to do that.
-// Maybe use the createSet operation in the DocumentClient and
+// Maybe use the teSet operation in the DocumentClient and
 // DynamoDB will handle everything else on the back end.
 // Each user has only one UserID, so that's unique.
 // Each user can have a bunch of lists, but each list name has to be unique
@@ -54,7 +54,47 @@ exports.handler = function (event, context, callback) {
 };
 
 var handlers = {
+    // Built in Amazon intents:
+    'AMAZON.HelpIntent': function () {
+        var speech = new Speech();
+        speech.say('The List of Lists skill allows you to maintain multiple lists with Alexa.');
+        speech.pause('250ms');
+        speech.say('To create a new list, say create list, and then the list name');
+        speech.pause('250ms');
+        speech.say('To add an entry to your active list, say: add entry followed by the entry name');
+        speech.pause('250ms');
+        speech.say('To switch to a different list say: change list to your list name');
+        speech.pause('250ms');
+        speech.say('To delete a list, say: delete list, and the name of the list to delete');
+        speech.pause('250ms');
+        speech.say('To remove an entry from a list, say: delete entry, followed by the entry name');
+        speech.pause('250ms');
+        speech.say('To say all of your lists, say: what are my lists');
+        speech.pause('250ms');
+        speech.say('To list the entries in your active list, say: what are my entries');
+        speech.pause('250ms');
+        speech.say('What would you like to do?')
+        var speechOutput = speech.ssml(true);
+        this.emit(':ask', speechOutput, 'Please say that again');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Amazon stop intent' + NOT_IMPLEMENTED);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Amazon cancel intent' + NOT_IMPLEMENTED);
+    },
     'LaunchRequest': function () {
+        // Works
+        if (Object.keys(this.attributes).length === 0) { // Check if it's the first time the skill has been invoked 
+            this.attributes['activeList'] = 'nothing';
+            this.emit(':tell', 'You currently have no active list.');
+        } else {
+            this.emit(':saveState', true);
+            this.emit(':tell', 'The current active list is ' + this.attributes['activeList']);
+        }
+    },
+    'openIntent': function () {
+        // Works
         if (Object.keys(this.attributes).length === 0) { // Check if it's the first time the skill has been invoked 
             this.attributes['activeList'] = 'nothing';
             this.emit(':tell', 'You currently have no active list.');
@@ -64,6 +104,7 @@ var handlers = {
         }
     },
     'addItemIntent': function () {
+        // Works
         var self = this;
         var listItem = this.event.request.intent.slots.listText.value;
         var userId = this.event.session.user.userId;
@@ -71,28 +112,35 @@ var handlers = {
         // Use the update method
         // listItems is a set, so we can use the ADD thing with the update
         // method. 
-        var params =  {
-            TableName: 'ListItems',
-            Key: {
-                'userId': userId,
-                'listName': myActiveList
-            },
-            AttributeUpdates: {
-                'listItems': {
-                    Action: "ADD",
-                    Value: docClient.createSet( listItem )
+        if (listItem) {
+            var params =  {
+                TableName: 'ListItems',
+                Key: {
+                    'userId': userId,
+                    'listName': myActiveList
+                },
+                AttributeUpdates: {
+                    'listItems': {
+                        Action: "ADD",
+                        Value: docClient.createSet( listItem )
+                    }
                 }
+            };
+            docClient.update( params, function(err, data) {
+                if (err) console.log(err);
+                else {
+                    console.log(data);
+                    self.emit(':tell', 'Added ' + listItem + ' to ' + myActiveList);
+                }
+            });
+        } else {
+            // Error condition where the listItem wasn't present.
+            // Could be made better by a conversational interaction
+                self.emit(':tell', 'I couldn\'t understand the item to add to the list. To add an item say: add entry, followed by the entry name.');
             }
-        };
-        docClient.update( params, function(err, data) {
-            if (err) console.log(err);
-            else {
-                console.log(data);
-                self.emit(':tell', 'Added ' + listItem + ' to ' + myActiveList);
-            }
-        });
     },
     'changeActiveListIntent': function () {
+        // Works
         // changes the active list. Doesn't create a new
         // entry in the ListItems table though. That is only
         // done in the addItemIntent
@@ -102,51 +150,33 @@ var handlers = {
         this.emit(':tell', 'Changing active list to ' + newList);
     },
     'deleteListIntent': function () {
-        // This will delete all the items in the myItems table
-        // for the activeList, then delete the list from the myLists table
-        // and set the activeList to nothing
+        // Works, but I would like to change the actions somewhat
+        // This will delete the list specified by the user
         var self = this;
+        var activeList = this.attributes['activeList'];
         var userId = this.event.session.user.userId;
-        var myList = this.attributes['activeList'];
-        // delete the list items first
+        var listToDelete = this.event.request.intent.slots.listName.value;
         var params = {
             TableName: "ListItems",
-            KeyConditionExpression: "userId = :u",
-            ExpressionAttributeValues: {
-                ":u": userId
+            Key: {
+                "userId": userId,
+                "listName": listToDelete
+            },
+        }
+        docClient.delete(params, function (err, data) {
+            if (err) {
+                console.log(err);
+                self.emit(':tell', "Unable to delete " + listToDelete);
+            } else {
+                // this is not exactly correct.
+                // I also would like to set the active list to nothing,
+                // and emit with state, but that wasn't working.
+                self.emit(':tell', 'Deleted list ' + listToDelete);
             }
-        };
-        docClient.query(params, function(err, data) {
-            if (err) console.log(err, err.stack);
-            else {
-                var theItem;
-                var theList;
-                data.Items.forEach( function (item) {
-                    var list_item = split(":", item.list_item);
-                    theItem = list_item[1];
-                    theList = list_item[0];
-                    // now do the delete
-                    if ( myList == theList ) {
-                        params = {
-                            TableName: "ListItems",
-                            Key: {
-                                "userId": userId,
-                                "list_item": item.list_item
-                            }
-                        }
-                        docClient.delete(params, function(err, data) {
-                            if (err) consol
-                        })
-                    }
-                    params = {
-                        TableName: "ListItems"
-                    }
-                });
-                    self.emit(':tell', 'Your saved lists are: ' + myResponse);
-                }
         });
     },
     'newListIntent': function () {
+        // Works
         // User wants to create a new list.
         // Implementation is the same as changeActiveListIntent
         var newList = this.event.request.intent.slots.listName.value;
@@ -155,37 +185,44 @@ var handlers = {
         this.emit(':tell', 'Changing active list to ' + newList);
     },
     'removeItemIntent': function () {
+        // Works.
         var self = this;
         var userId = this.event.session.user.userId;
-        var myItem = this.event.request.intent.slots.listItem.value;
+        var listItem = this.event.request.intent.slots.listItem.value;
         var myList = this.attributes['activeList'];
-        var list_item = myList + ':' + myItem;
-        var params = {
-            TableName: "ListItems",
+        var params =  {
+            TableName: 'ListItems',
             Key: {
-                "userId": userId,
-                "list_item": list_item
+                'userId': userId,
+                'listName': myList
             },
-            ConditionExpression:"list_item = :val",
-            ExpressionAttributeValues: {
-                ":val": list_item
+            AttributeUpdates: {
+                'listItems': {
+                    Action: "DELETE",
+                    Value: docClient.createSet(listItem)
+                }
             }
-        }
-
-        docClient.delete(params, function(err, data) {
+        };
+        docClient.update( params, function(err, data) {
             if (err) {
                 console.log(err);
-                self.emit(':tell', "Unable to delete " + myItem + " from " + myList);
-            } else {
-                self.emit(':tell', 'Item ' + myItem + ' deleted from ' + myList);
+                self.emit(':tell', 'There was an error deleting ' + listItem + ' from ' + myList);
+            }
+            else {
+                console.log(data);
+                self.emit(':tell', 'Deleted ' + listItem + ' from ' + myList);
             }
         });
     },
     'sayAllListsIntent': function () {
+        // Works
+        // Need a slight adjustment on the response for the commas in the list.
+        // Would also like to add some speech enhancements like pauses.
         var self = this;          
         var userId = this.event.session.user.userId;
+        var myActiveList = this.attributes['activeList'];
         var params = {
-            TableName: "myLists",
+            TableName: "ListItems",
             KeyConditionExpression: "userId = :u",
             ExpressionAttributeValues: {
                 ":u": userId
@@ -196,35 +233,44 @@ var handlers = {
             else {
                     var myResponse = "";
                     data.Items.forEach( function (item) {
-                        myResponse = myResponse + ', ' + item.listName;
+                        myResponse = item.listName + ', ' + myResponse;
                     });
-                    self.emit(':tell', 'Your saved lists are: ' + myResponse);
+                    self.emit(':tell', 'Your saved lists are: ' + myResponse + ' Current active list is ' + myActiveList);
                 }
         });
     },
     'sayListIntent': function () {
+        // Works
+        // Still a slight mod needs to be made to fix the comma at the end of the list
+        // and then maybe add some pauses in.
         var self = this;
         var userId = this.event.session.user.userId;
         var myList = this.attributes['activeList'];
         var params = {
             TableName: "ListItems",
-            KeyConditionExpression: "userId = :u",
+            KeyConditionExpression: "userId = :u and listName = :val",
             ExpressionAttributeValues: {
-                ":u": userId
+                ":u": userId,
+                ":val": myList
             }
         };
         docClient.query(params, function(err, data) {
-            if (err) console.log(err, err.stack);
+            if (err) {
+                console.log(err, err.stack);
+                self.emit(':tell', 'Error in the list query');
+            }
             else {
+                // There will always be only 1 thing returned
                 var myResponse = "";
+                console.log(data);
                 data.Items.forEach( function (item) {
-                    var entry = item.list_item.split(":");
-                    if (entry[0] == myList ) {
-                        // get the item, and add it to the response
-                        myResponse = myResponse + ', ' + entry[1];
-                    }
+                    console.log(item);
+                    item.listItems.values.forEach( function (entry) {
+                        console.log(entry);
+                        myResponse = entry + ', ' + myResponse;
+                    });
                 });
-                self.emit(':tell', 'Active list is: ' + myList + ' The items in your active list are: ' + myResponse);
+                self.emit(':tell', 'Items in list ' + myList + ' are: ' + myResponse);
             }
         });
     }
